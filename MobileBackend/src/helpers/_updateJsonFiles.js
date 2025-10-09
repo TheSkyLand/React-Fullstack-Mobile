d9e8481377bad0708fbf52a9fe13ffddc98a8754
George <77775699+TheSkyLand@users.noreply.github.com>
2025-10-09 15:08:39 +0300

    Delete MobileBackend directory

--- MobileBackend/src/helpers/_updateJsonFiles.js
+++
@@ -1,8 +0,0 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-
-const fs = require('fs');
-
-export const updateJsonFile = (file, data) => {
-    fs.writeFileSync(`data/${file}`, JSON.stringify(data, null, 4)); // JSON.stringify(data) - форматирует json-файл в строку
-}
