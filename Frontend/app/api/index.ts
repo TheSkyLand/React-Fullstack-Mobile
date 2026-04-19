9f0b0d31b2f6e7a458b25c82bc8caed77d6e48a0
TheSkyLand <Aradioactiveplay@gmail.com>
2026-04-20 00:21:38 +0300

    final

--- Frontend/app/api/index.ts
+++ Frontend/app/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const $api = axios.create({
-    baseURL: `http://localhost:3001`,
+    baseURL: `http://10.0.2.2:3001`,
 });
 
 export const config = () => {
--- MobileBackend/index.js
+++ MobileBackend/index.js
@@ -5,13 +5,17 @@ const require = createRequire(import.meta.url);
 
 const express = require("express");
 
+
+
+
 import { shopApi } from "./src/api/_shopApi.js";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-
+const cors = require('cors');
+app.use(cors()); // разрешает всё
 shopApi(app)
 app.listen(PORT, () => {
 	console.log(`Server starting on port ${PORT}`);
